f63af5036dca5a1435f470f28bb6b422e903b369
okuluws <okuluws22@gmail.com>
2024-02-19 20:23:35 +0100

    removed useless code

--- src/routes/api_hm/+server.ts
+++ src/routes/api_hm/+server.ts
@@ -1,17 +1,8 @@
 import { json } from '@sveltejs/kit';
 
-let a = 0;
 export const POST = async ({ request, getClientAddress }) => {
     console.log(new Date().toISOString());
 	const { redstone_power } = await request.json();
     
 	return json("thanks for post");
-};
-
-export const GET = async () => {
-    console.log("received get");
-    a += 1;
-    console.log(a);
-    
-	return json("hello get");
 };
