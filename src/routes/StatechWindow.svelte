a4f4b0e262b1a4cf85175260ca05f8e719378f1e
okuluws <okuluws22@gmail.com>
2024-02-20 17:49:14 +0100

    moved api to db, made statech window work maybe

--- src/routes/StatechWindow.svelte
+++ src/routes/StatechWindow.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
     import MyWindow from "$lib/MyWindow.svelte";
+    import { pb } from "$lib/pocketbase";
+    import { onDestroy, onMount } from "svelte";
 
-    let powerlevel: number
+
+    let power: number;
+    let unsubscribe: () => void;
+    onMount(async () => {
+        unsubscribe = await pb.collection("global_data").subscribe("*", async ({ record }) => {
+            power = JSON.parse(record.data).power
+        });
+    });
+
+    onDestroy(() => {
+        unsubscribe()
+    });
 </script>
 <MyWindow display_text={"Statech"}>
-    Current Powerlevel: {powerlevel}
+    Current Power: {power}
 </MyWindow>
