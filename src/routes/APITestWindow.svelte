abbf108e094fdef6f02549b971709210fb65f2bc
okuluws <okuluws22@gmail.com>
2024-02-20 16:01:52 +0100

    added header field for api test

--- src/routes/APITestWindow.svelte
+++ src/routes/APITestWindow.svelte
@@ -4,12 +4,16 @@
     let route: string = "";
     let response: Response;
     let post_body: string;
+    let headers: string;
 </script>
 <MyWindow display_text={"API Test"}>
     <input type="text" placeholder="Route" bind:value={route}>
+    <input type="text" placeholder="Headers" bind:value={headers}>
+    <br/>
     <button class="hover:bg-slate-300" on:click={async () => response = await fetch(route)}>Send Get</button>
     <br/>
     <input type="text" placeholder="POST Data" bind:value={post_body}>
+    <br>
     <button class="hover:bg-slate-300" on:click={async () => response = await fetch(route, { method: "POST", body: post_body})}>Send Post</button>
     <br/>
     Status: {response?.status ?? ""}
