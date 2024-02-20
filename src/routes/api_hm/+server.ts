25984bc47beb67c7c33884cfe6b494bd2e9e952c
okuluws <okuluws22@gmail.com>
2024-02-20 14:50:28 +0100

    made api actually work and cleaner, also added api key

--- src/routes/api_hm/+server.ts
+++ src/routes/api_hm/+server.ts
@@ -1,7 +1,14 @@
+import { API_KEY } from '$env/static/private';
 import { json } from '@sveltejs/kit';
 
 export const POST = async ({ request, getClientAddress }) => {
-    console.log("[" + new Date().toUTCString() + " | " + getClientAddress() + "] " + await request.text());
-    
+    console.log(`[${new Date().toUTCString()}][${getClientAddress()}][POST]`);
+    const req = await request.json();
+    if (req.API_KEY !== API_KEY) {
+        console.log("api key was wrong/missing");
+        return json("api key was wrong/missing");
+    }
+
+    console.log(`power: ${req.power}`)
 	return json("thanks for post");
 };
