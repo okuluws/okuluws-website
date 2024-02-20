fc8d44df4a58de52d1f0c67471c0197c444c2be7
okuluws <okuluws22@gmail.com>
2024-02-20 17:52:30 +0100

    forgor to set ip of db

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase from "pocketbase";
 import { writable } from "svelte/store";
 
-export const pb = new PocketBase("http://172.18.37.33:8090");
+export const pb = new PocketBase("http://212.132.76.59:8090/");
 export const current_user = writable(pb.authStore.model);
 pb.authStore.onChange(() => {
     current_user.set(pb.authStore.model);
