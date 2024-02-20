2ab4642fdaca10174ab98d12a5c04749129ea465
okuluws <okuluws22@gmail.com>
2024-02-20 15:28:03 +0100

    debug cause it wont work

--- src/routes/api_hm/+server.ts
+++ src/routes/api_hm/+server.ts
@@ -3,6 +3,7 @@ import { json } from '@sveltejs/kit';
 
 export const POST = async ({ request, getClientAddress }) => {
     console.log(`[${new Date().toUTCString()}][${getClientAddress()}][POST]`);
+    console.log(await request.text())
     const req = await request.json();
     if (req.API_KEY !== API_KEY) {
         console.log("api key was wrong/missing");
