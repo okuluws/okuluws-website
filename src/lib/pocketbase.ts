f79509651a3a316286420ad8a30cfe0a6d0145c1
okuluws <okuluws22@gmail.com>
2024-02-21 19:06:46 +0100

    fixed database url

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase from "pocketbase";
 import { writable } from "svelte/store";
 
-export const pb = new PocketBase("http://212.132.76.59:8090/");
+export const pb = new PocketBase("http://okuluws.de:6969/");
 export const current_user = writable(pb.authStore.model);
 pb.authStore.onChange(() => {
     current_user.set(pb.authStore.model);
