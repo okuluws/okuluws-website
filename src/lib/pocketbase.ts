185f9887c95006d8a6c131b5256b1c2ffc2329f6
okuluws <okuluws22@gmail.com>
2024-02-22 15:45:04 +0100

    hm

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase from "pocketbase";
 import { writable } from "svelte/store";
 
-export const pb = new PocketBase("http://okuluws.de:6969/");
+export const pb = new PocketBase("https://okuluws.de:6969/");
 export const current_user = writable(pb.authStore.model);
 pb.authStore.onChange(() => {
     current_user.set(pb.authStore.model);
