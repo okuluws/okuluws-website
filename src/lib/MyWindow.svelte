4ee049a783ac097278eaf5275921738bf6164f7b
okuluws <okuluws22@gmail.com>
2024-03-09 11:25:23 +0100

    added window layering

--- src/lib/MyWindow.svelte
+++ src/lib/MyWindow.svelte
@@ -2,6 +2,8 @@
     import SVG_close from "$lib/svg/close-svgrepo-com (1).svelte";
     import SVG_minimize from "$lib/svg/minus-svgrepo-com.svelte";
     import SVG_normalize from "$lib/svg/plus-large-svgrepo-com.svelte";
+    import { windows_z_ordering } from "./MyWindow";
+    import { get } from 'svelte/store';
 
     
     export let left: number = 100;
@@ -53,15 +55,33 @@
         moving = false;
     }
 
-    let is_focused: boolean = false;
-
     export let display_state: "open" | "minimized" | "closed" = "open";
     export let display_text: string = "Unnamed Window";
 
+    export let id: string = get(windows_z_ordering).length.toString();
+    windows_z_ordering.update(items => {
+        items.push(id);
+        return items;
+    });
+
+    export let z_index: number = 0;
+    windows_z_ordering.subscribe((items) => {
+        z_index = items.indexOf(id);
+    });
+
+    function move_z_index() {
+        windows_z_ordering.update(items => {
+            return items.filter(_id => _id !== id);
+        });
+        windows_z_ordering.update(items => {
+            items.push(id);
+            return items;
+        });
+    }
 </script>
 
 {#if display_state == "open" || display_state == "minimized"}
-<button class="absolute border self-start cursor-auto select-text touch-none" style="left: {left}px; top: {top}px; z-index: {is_focused ? "1" : "0"};" on:focusin={() => is_focused = true} on:focusout={() => is_focused = false}>
+<button class="absolute border self-start cursor-auto select-text touch-none" style="left: {left}px; top: {top}px; z-index: {z_index};" on:mousedown={move_z_index} on:touchstart={move_z_index}>
     <div class="h-6 flex bg-gray-100">
         <button class="h-full px-1 hover:bg-slate-300 select-none text-left grow overflow-hidden" on:mousedown={onMouseDown} on:touchstart={onTouchStart}>
             {display_text}
---
+++ src/lib/MyWindow.ts
@@ -0,0 +1,4 @@
+import { writable } from 'svelte/store';
+
+
+export const windows_z_ordering = writable<Array<string>>([]);
